71a5c4ce2abc026df1c3de32bc7ab84f2663010c
Mike Varga <mikevarga07@gmail.com>
2022-12-23 22:18:49 -0600

    made a quiz game using OOP.

---
+++ main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz\nYour final score was {quiz.score}/{quiz.question_number}")
